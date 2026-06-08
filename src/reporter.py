bc02cdc17ddce1554d2939f4ffd7231ee4d31811
Nathan Santos <natesantosa@gmail.com>
2026-06-08 11:47:17 -0700

    add cross-reference data for VT and AbuseIPDB results to filter verdict calls based on conditons met by both tools

--- src/reporter.py
+++ src/reporter.py
@@ -1,38 +1,41 @@
 # Report generator to confirm findings of VT API 
 
-import os
 import csv
-import time
-from dotenv import load_dotenv
-import requests
-from parser import get_logs, network_events
-from extractor import extract_ioc
-from virustotal import check_ip
 
-def generate_report(result):
+def generate_report(result, choice):
     with open('output/report.csv', 'w') as file:
         writer = csv.writer(file)
-        writer.writerow(['IP', 'Malicious', 'Suspicious', 'Harmless', 'Result'])
         # Loop through each iteration of a new IP and subsequent results and add new CSV line
-        for ip in result:
-            stats = result[ip]
-            if stats['malicious'] >= 1:
-                writer.writerow([ip, stats['malicious'], stats['suspicious'], stats['harmless'], "FLAGGED"])
-            else:
-                writer.writerow([ip, stats['malicious'], stats['suspicious'], stats['harmless'], "CLEAN"])
+        if choice == "A":
+            writer.writerow(['Type', 'IOC', 'Malicious', 'Suspicious', 'Harmless', 'AbuseIPDB Score','Threat'])
+            for ip in result:
+                stats = result[ip]
+                # Fix flagging condition
+                if (stats['malicious'] >= 1 or stats['suspicious'] >= 2) and stats['confidence'] >= 50:
+                    verdict = "FLAGGED/HIGH"
+                elif (stats['malicious'] >= 1 or stats['suspicious'] >= 1) and stats['confidence'] >= 20:
+                    verdict = "SUSPICIOUS"
+                elif stats['isTor'] is True:
+                    verdict = "SUSPICIOUS (Tor)"
+                else:
+                    verdict = "CLEAN"
+                writer.writerow(['IP', ip, stats['malicious'], stats['suspicious'], stats['harmless'], stats['confidence'], verdict])
+        elif choice == "B":
+            writer.writerow(['Type', 'IOC', 'Malicious', 'Suspicious', 'Harmless','Result'])
+            for file_hash in result:
+                stats = result[file_hash]
+                if stats['malicious'] >= 1 or stats['suspicious'] >= 2:
+                    verdict = "FLAGGED"
+                else:
+                    verdict = "CLEAN"
+                writer.writerow(['HASH', file_hash, stats['malicious'], stats['suspicious'], stats['harmless'], "CLEAN"])
 
-if __name__ == "__main__":
-    events = get_logs('sample_logs/sample_alerts.json')
-    net_evts = network_events(events)
-    ip_list = extract_ioc(net_evts)
-    result_dict = {}
-    for i in ip_list:
-        print(f"Checking IP: {i}... ")
-        result = check_ip(i)
-        result_dict[i] = result
-        time.sleep(15)
-    generate_report(result_dict)
-
+def main_menu():
+    print("Log Parser & IOC Scanner Menu")
+    print("Select from choices: ")
+    print("A) IP Check")
+    print("B) Hash Check")
+    print("Q) Quit")
     
 
             
